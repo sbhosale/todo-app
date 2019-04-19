06cd62122d7abe75456ad58b061fd45e82d4599f
Sushant Bhosale <sushant.bhosale@gmail.com>
2019-04-18 20:52:59 -0400

    Component and Template for ToDo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  newTodo: Todo = new Todo();
+
+  constructor(private _todoDataService: TodoDataService) {}
+
+  addTodo() {
+    this._todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this._todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this._todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this._todoDataService.getAllTodos();
+  }
 }
